6d8a01b8f99d697d65e319c08dbdc19d4d58d849
Priya Pandey <pp1549660@gmail.com>
2025-06-21 00:33:14 +0000

    container configuration done

---
+++ datatype2.js
@@ -0,0 +1,10 @@
+"Use Strict"; //treat all js code as newer version 
+let name ="Priya"
+let Age =22
+let isloggedIn =false
+console.log(Age)
+console.log(typeof name);
+console.log(typeof null);// it is a object 
+console.log(typeof undefined); // undefined is also a datatype 
+
+
