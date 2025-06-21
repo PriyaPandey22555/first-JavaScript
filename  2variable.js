9ee9214215888915133f35e5f0bfc4c7a75d68cf
Priya Pandey <pp1549660@gmail.com>
2025-06-21 00:08:26 +0000

    container configuration done

---  2variable.js
+++
@@ -1,6 +0,0 @@
-const accountId =2204981
-let accountEmail =" Priya942@gmail.accountEmail"
-var accountPassword ="12345"
-accountcity = "Bihar"
-accountId =2
-console.log(accountId);
--- Variable2.js
+++ Variable2.js
@@ -2,5 +2,10 @@ const accountId =2204981
 let accountEmail =" Priya942@gmail.accountEmail"
 var accountPassword ="12345"
 accountcity = "Bihar"
-accountId =2
+// accountId =2
+let accountState;
+accountEmail="priyawemmf@gmail.com"
+accountPassword="3234657"
+accountcity="Mumbai"
 console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountcity,accountState]);
