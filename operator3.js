82d9be8bf6e6e209e35049477961ea879c4a4a86
Priya Pandey <pp1549660@gmail.com>
2025-06-22 00:20:36 +0000

    container configuration done

---
+++ operator3.js
@@ -0,0 +1,12 @@
+let num1 = 3
+let num2 = 4 
+
+console.log(num1 + num2)
+console.log(num1 * num2)
+console.log(num1 - num2)
+console.log(num1 ** num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+console.log("3" + num2)
+console.log(3+ "num2" + 2)
+console.log(num1 + num2 + "2")
