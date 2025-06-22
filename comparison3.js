99607695710fbcd3672e43086cc422ef10710710
Priya Pandey <pp1549660@gmail.com>
2025-06-22 00:43:29 +0000

    container configuration done

---
+++ comparison3.js
@@ -0,0 +1,21 @@
+// console.log(2 ==1);
+// console.log(2 >1);
+// console.log(2 >=1);
+// console.log(2 <1);
+// console.log(2 <=1);
+// console.log(2 !=1);
+// console.log(2 ===1);
+// console.log("2"> 1);
+// console.log("02" >1);
+// console.log("2 "===1);
+// comparison between different data type is not posible but it give the answer by automatically change the datatype of give variable
+// console.log(null > 0)
+   
+
+console.log( null == 0);
+
+console.log(null > 0);
+
+console.log(null >= 0);
+
+
