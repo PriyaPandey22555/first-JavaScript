3fe803a0010067046c763b56eff2bde9df4f9bcd
Priya Pandey <pp1549660@gmail.com>
2025-06-23 23:54:50 +0000

    container configuration done

---
+++ String.js
@@ -0,0 +1,22 @@
+const name ="Priya"
+const repoCount = 50
+// console.log(name+repoCount+"value")
+// console.log('Hello my name is ${name} and my repo count is ${repoCount}');
+const gameName = new String('Priya-Kumari-Pandey') 
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));
+// const newString = gameName.substring(0,4)
+// console.log(newString)
+// const anotherString = gameName.slice(-1,2)
+// console.log(anotherString);
+// const newStringOne = "   Priya    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+const url = "https://Priya.com/priya%34Pandey"
+console.log(url.replace('%34' , '-'));
+console.log(url.includes('Sakshi'));
+console.log(gameName.split('-'));
