12ec75e2548bd63e4cc285fdb04613558b5fac0e
Priya Pandey <pp1549660@gmail.com>
2025-06-30 08:41:07 +0000

    container configuration done

---
+++ NumberAndMath.js
@@ -0,0 +1,26 @@
+// const score = 400;
+// console.log(score);
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+// const otherNumber =23.564
+// console.log(otherNumber.toPrecision(14));
+// const hundred = 1000000;
+// console.log(hundred.toLocaleString());
+// console.log(hundred.toLocaleString('en-IN'));
+//++++++++++++++++++++++ Math++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(4,5,6,7,2));
+// console.log(Math.max(4,5,6,4,2,5));
+console.log(Math.random());
+console.log((Math.random()*10) );
+console.log((Math.random()*10)+1 );
+console.log(Math.floor(Math.random()*10)+1);
+const min =10;
+const max = 20;
+console.log(Math.floor(Math.random() *(max -min +1)));
