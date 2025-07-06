632b933b07240aa3007a4d18e9ef2ed9b3b8c59a
Priya Pandey <pp1549660@gmail.com>
2025-07-06 09:08:00 +0000

    container configuration done

--- array.js
+++ array.js
@@ -40,5 +40,17 @@ const dc_heros = ["superman","flash","batman"]
 // console.log(allheros);
 // console.log(allheros.length);
 // +++++sparated
-const all_new_heros = [...marvel_heros,...dc_heros]
-// console.log(all_new_heros);
+// const all_new_heros = [...marvel_heros,...dc_heros]
+// // console.log(all_new_heros);
+// const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array = another_array.flat(Infinity)
+// const real_another_array1 = another_array.flat(2)
+// console.log(real_another_array);
+// console.log(real_another_array1);
+// console.log(Array.isArray("Priya"))
+// console.log(Array.from("Priya"))
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
+
